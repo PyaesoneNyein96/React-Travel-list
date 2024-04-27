0a969e2220327e0e89e6471bb4e1347669881074
pyaesonenyein <pyaesonenyein1996@gmail.com>
2024-04-28 03:12:07 +0630

    lesson 1:56:56

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 1, packed: false },
+  { id: 2, description: "show money", quantity: "30k baht", packed: false },
+  { id: 3, description: "Laptop", quantity: 1, packed: true },
+  { id: 3, description: "Phone", quantity: 1, packed: true },
+  { id: 3, description: "Programming Skill", quantity: "10+", packed: true },
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div>
+    <Logo />
+    <Form />
+    <PackageList />
+    <Stats />
+  </div>
 }
 
-export default App;
+
+
+function Logo() {
+  return <h1>🛫 Road to Thailand 💻 </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your Thailand trip.</h3>
+    </div>
+  )
+}
+
+
+function PackageList() {
+  return (
+    <div className="list">
+      <ul >
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span >
+      <button> ❌ </button>
+    </li>
+  )
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        You have X items on your list, and you already packed X (X%)
+      </em>
+    </footer>
+  )
+}
